2cb54453296b23d5e5a0d0a2e0752693cd6487bd
Joaquin PC-2 <joaquinbarrandeguy@gmail.com>
2024-12-08 08:59:32 -0300

    Transaction 1.4 SSE

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  experimental: {
+    // Disable static generation for this specific API route
+    serverComponentsExternalPackages: ['next/server'],
+  },
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
